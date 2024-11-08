85274b4c73cee2c774a6e36339481b5dc4a7de12
fenek-dev <makstreonin@gmail.com>
2024-11-08 19:25:48 +0300

    Basic tokenizer

--- fnk_compile.c
+++ fnk_compile.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -12,7 +13,6 @@ typedef struct BufHeader {
     size_t cap;
     char buf[0];
 } BufHeader;
-// 0000 0001 000000000000
 
 #define buf__hdr(b) ((BufHeader *)((char *)b - offsetof(BufHeader, buf)))
 #define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
@@ -41,7 +41,7 @@ void *buf__grow(const void *buf, const size_t new_len, const size_t elem_size) {
 void buf_test() {
     int *buf = NULL;
 
-    enum { N = 1024};
+    enum { N = 1024 };
     for (int i = 0; i < N; ++i) {
         buf_push(buf, i);
     }
@@ -54,9 +54,151 @@ void buf_test() {
     buf_free(buf);
 }
 
-int main(int args, char **argv) {
+typedef enum TokenKind {
+    TOKEN_INT = 128,
+    TOKEN_NAME
+} TokenKind;
 
+typedef struct Token {
+    TokenKind kind;
+
+    union {
+        uint64_t val;
+
+        struct {
+            const char *start;
+            const char *end;
+        };
+    };
+} Token;
+
+char *stream;
+Token token;
+
+void next_token() {
+    switch (*stream) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9': {
+            uint64_t val = 0;
+            while (isdigit(*stream)) {
+                val *= 10;
+                val += *stream++ - '0';
+            }
+            token.kind = TOKEN_INT;
+            token.val = val;
+            break;
+        }
+
+
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'I':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'U':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+        case '_': {
+            const char *start = stream;
+            while (isalnum(*stream) || *stream == '_') {
+                stream++;
+            }
+            token.start = start;
+            token.end = stream;
+            token.kind = TOKEN_NAME;
+
+            break;
+        }
+
+
+        default:
+            token.kind = *stream++;
+            break;
+    }
+}
+
+void print_token(const Token token) {
+    switch (token.kind) {
+        case TOKEN_INT:
+            printf("INT VALUE: %llu\n", token.val);
+            break;
+        case TOKEN_NAME:
+            printf("NAME VALUE: %.*s\n", (int)(token.end - token.start), token.start);
+            break;
+        default:
+            printf("UNKNOWN VALUE %c\n", token.kind);
+            break;
+    }
+}
+
+void lex_test() {
+    char *source = "+()_well1234+HelloWorld_994";
+    stream = source;
+
+
+    next_token();
+    while (token.kind) {
+        print_token(token);
+        next_token();
+    }
+}
+
+
+int main(int args, char **argv) {
     buf_test();
+    lex_test();
 
     return 0;
 }
